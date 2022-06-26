e6070c3894a09909b983e65e356752319d396ddd
zaynlannister <zaynlannister@gmail.com>
2022-06-26 15:18:39 +0500

    sync localStorage

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,19 @@
 import '../scss/app.scss';
-// import './demo.js';
 
+let domElements = {
+    subButton: document.querySelector('.footer__actions-button button'),
+    container: document.querySelector('.footer__actions')
+}
 
-console.log('привет')
+if (localStorage.getItem('follower')) {
+    changeContent(domElements.container, `<div class="footer__actions-sub-text">Спасибо за подписку!</div>`);
+}
+
+domElements.subButton.addEventListener('click', () => {
+    changeContent(domElements.container, `<div class="footer__actions-sub-text">Спасибо за подписку!</div>`);
+    localStorage.setItem('follower', 'true')
+})
+
+function changeContent(element, data) {
+    element.innerHTML = data;
+}
